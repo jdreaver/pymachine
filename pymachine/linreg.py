e100fe6177f98a6c0ce9191dabe532a1295570a9
John David Reaver <jdreaver@adlerhorst.com>
2013-02-14 17:23:06 -0800

    Added linear regression and regularized version

--- pymachine/linreg.py
+++ pymachine/linreg.py
@@ -1,3 +1,18 @@
 """This module implements linear regression."""
 
 import numpy as np
+
+
+def linear_regression(X, y):
+    
+    """Performs simple linear regression."""
+
+    return np.linalg.lstsq(X, y)[0]
+
+def weight_decay_regression(X, y, decay):
+    
+    """Performs linear regression with weight decay."""
+
+    (N, dim) = X.shape
+    return np.linalg.solve(np.dot(X.T, X) + decay * np.identity(dim), 
+                           np.dot(X.T, y))
